a8994d77c52ce38a52cb7af95d6d91b9f520d317
Florian Hamel <fhamel.freelance@gmail.com>
2023-05-03 13:48:37 +0200

    terminal writing about TBD

--- src/app/components/about/about-topic/about-topic.component.ts
+++ src/app/components/about/about-topic/about-topic.component.ts
@@ -1,14 +1,24 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ITopic} from "../about.component";
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-about-topic',
   templateUrl: './about-topic.component.html',
   styleUrls: ['./about-topic.component.less']
 })
-export class AboutTopicComponent {
+export class AboutTopicComponent implements OnInit {
   @Input() topic!: ITopic;
   isCommandRun: boolean = false;
+  text: string = '';
+  textInterval: Subscription = new Subscription();
+
+  ngOnInit() {
+    this.textInterval = interval(0.25).subscribe(() => {
+        this.text += this.topic.text.charAt(this.text.length);
+      }
+    )
+  }
 
   buildContent(): void {
     this.isCommandRun = !this.isCommandRun;
